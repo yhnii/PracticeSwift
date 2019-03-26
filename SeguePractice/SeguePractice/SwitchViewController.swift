badf293ffde5a78cfc8a0a915de1efb9d9166c96
yhnii <lyh.data@gmail.com>
2019-03-26 17:24:05 +0900

    두 개 이상의 뷰를 연결하고 값을 전달하는 연습을 해 보았습니다.

---
+++ SeguePractice/SeguePractice/SwitchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SwitchViewController.swift
+//  SeguePractice
+//
+//  Created by SWUCOMPUTER on 26/03/2019.
+//  Copyright © 2019 SWUCOMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class SwitchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SeguePractice/SeguePractice/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SeguePractice
+//
+//  Created by SWUCOMPUTER on 26/03/2019.
+//  Copyright © 2019 SWUCOMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var onOffSwitch: UISwitch!
+    @IBOutlet var leftRightSegControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+    }
+
+}
+
